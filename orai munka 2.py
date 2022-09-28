49fc3511b476753fd23871dc95202de87bcc3d76
mitroba <barnabasmitro@gmail.com>
2022-09-28 12:31:35 +0200

    09.28 orai munka 2

---
+++ orai munka 2.py
@@ -0,0 +1,11 @@
+veznev=input("Kérem adja meg a vezetéknevét:")
+
+kernev=input("Kérem adja meg a keresznevét:")
+
+ev=int(input("Kérem adja meg a szültési évét:"))
+
+print(29*"*")
+
+print("Az ön teljes neve:", veznev, kernev,)
+
+print("Az életkora:", 2022-ev)
