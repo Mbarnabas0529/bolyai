68fd289344acadf3217dd23e4052dba4455190e6
Mbarnabas05 <barnabasmitro@gmail.com>
2022-10-03 15:13:10 +0200

    2022.10.02

--- hazi doga 4.py
+++ hazi doga 4.py
@@ -12,6 +12,6 @@ ideal2 =round(ideal2,2)
 
 print("*"*30,"-"*5,"*"*30, sep="")
 print("Az ön testtömegindexe:" , round(bmi,2),"%")
-print("Az ön magasságához az ideális testtömeg:",ideal1,"kg","és",ideal2,"kg")
+print("Az ön magasságához az ideális testtömeg:",ideal1,"kg","és",ideal2,"kg között van")
 
 
