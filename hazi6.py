12dee9ed9fdcf1becf27632cadaa6f8b35714aac
Mbarnabas05 <barnabasmitro@gmail.com>
2022-10-17 14:43:07 +0200

    2022.10.17 hazi 6

--- hazi6.py
+++ hazi6.py
@@ -1,10 +1,10 @@
-szam = int(input("Adj meg egy számot: "))
-szam2 = 0
-while szam != 1:
-    if szam % 2 == 0:
-        szam2 += 1
-        szam = szam / 2
-    elif szam % 2 == 1:
-        szam2 += 1
-        szam = szam + 1
+szam=int(input("Adj meg egy számot: "))
+szam2=0
+while szam!=1:
+    if szam%2==0:
+        szam2+=1
+        szam=szam / 2
+    elif szam%2 == 1:
+        szam2+=1
+        szam=szam + 1
     print("A", szam2, "elem: ", int(szam))
