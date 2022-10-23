7d4f37b8931815ed77ac0395cd1c0679fb40fae3
Mbarnabas0529 <barnabasmitro@gmail.com>
2022-10-23 10:04:18 +0200

    10.23 hazi 7

---
+++ hazi7.py
@@ -0,0 +1,11 @@
+dec = int(input("adj meg egy számot:"))
+bin = 0
+a = 0
+b = dec
+
+while(b > 0):
+    bin = ((b%2)*(10**a)) + bin
+    b = int(b/2)
+    a += 1
+
+print("A szám bináris alakja:",bin)
