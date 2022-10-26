63770be8273f53059a0958f99bb2b8cbf7570a48
mitroba <barnabasmitro@gmail.com>
2022-10-26 12:30:55 +0200

    2022.10.25 om7

--- om7.py
+++ om7.py
@@ -28,11 +28,34 @@ for i in range(7,-1,-1):
 print(kettes)
 print(30*"-")
 d = int(input("Adj meg egy pozitív egész számot:"))
-prim=True
+prim = True
 for i in range(2,d//2+1):
-    if d%i==0:
-        prim=False
+    if d%i == 0:
+        prim = False
         print("Az %i osztója %i-nek"%(i,d))
 if prim:
      print("A %i primszam" %(d))
+
+print(30*"-")
+e = int(input("Írj be egy pozitív egész számot:"))
+prim = True
+osszeg=0
+for i in range(2,e//2+1):
+    if e%i == 0:
+        prim = False
+        osszeg=osszeg+i
+        print("Az %i osztója %i-nek"%(i,e))
+print("Az osztóinak összege:",osszeg)
+print(30*"-")
+dec = int(input("adj meg egy számot:"))
+bin = 0
+a = 0
+b = dec
+
+while b > 0:
+    bin = ((b%2)*(10**a)) + bin
+    b = int(b/2)
+    a += 1
+
+print("A szám bináris alakja:",bin)
 
