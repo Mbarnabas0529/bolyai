7279b5e4f4a3367a2d841a7ed342ed2fd1a9fe75
Mbarnabas0529 <barnabasmitro@gmail.com>
2022-11-07 16:41:33 +0100

    11.07. Hazi 8

--- hazi 8.py
+++ hazi 8.py
@@ -1,11 +1,17 @@
-dec1 = int(input("Irj be a 1. oktet értékét az íp címből (0-255):"))
-dec2 = int(input("Irj be a 1. oktet értékét az íp címből (0-255):"))
-dec3 = int(input("Irj be a 1. oktet értékét az íp címből (0-255):"))
-dec4 = int(input("Irj be a 1. oktet értékét az íp címből (0-255):"))
+dec1 = int(input("Irja be az 1. oktet értékét az íp címből (0-255):"))
+dec2 = int(input("Irja be a 2. oktet értékét az íp címből (0-255):"))
+dec3 = int(input("Irja be a 3. oktet értékét az íp címből (0-255):"))
+dec4 = int(input("Irja be a 4. oktet értékét az íp címből (0-255):"))
+if dec1 or dec2 or dec3 or dec4 > 255:
+    print("Ne adj meg nagyobb értéket 😠")
 bin1 = ""
 bin2 = ""
 bin3 = ""
 bin4 = ""
+o1 = dec1   #????????????????????
+o2 = dec2   #????????????????????
+o3 = dec3   #????????????????????
+o4 = dec4   #????????????????????
 for i in range(7, -1, -1):
     if dec1 >= 0:
         bin1 = (str(dec1 % 2) + bin1)
@@ -22,4 +28,6 @@ for i in range(7, -1, -1):
     if dec4 >= 0:
         bin4 = (str(dec4 % 2) + bin4)
         dec4 = dec4 // 2
-print(dec1,dec2,dec3,dec4,"ipv4 cím kettes számrendszerben kifejezve =",bin1,bin2,bin3,bin4, sep=".")
+print(o1,o2,o3,o4,"ipv4 cím kettes számrendszerben kifejezve =",bin1,bin2,bin3,bin4, sep=".")
+fel2 = str(input("Írja be az ipv4-es címet (pl.:192.168.17.2):"))
+print(fel2, "ipv4 cím kettes számrendszerben kifejezve =")
