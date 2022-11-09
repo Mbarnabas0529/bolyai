cdbe2982c43742a16911beefa0a73927394a7e7b
mitroba <barnabasmitro@gmail.com>
2022-11-09 12:27:39 +0100

    2022.11.09 OOOMMM

---
+++ lista2.py
@@ -0,0 +1,12 @@
+szavak = ['sonka', 'sajt', 'lekvár', 'csokoládé']
+szavak [2:2]=["méz","cukor", 16]
+print(szavak)
+lista = ['sonka', 'sajt', 'lekvár', 'csokoládé']
+lista[1:3] = ['saláta','csirke','máj']
+print(lista)
+listam = ['sonka', 'sajt', 'lekvár', 'csokoládé']
+total = 0
+for szam in listam:
+    total += szam
+    print(total)
+
