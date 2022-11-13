9c179120a388997b651c6ea68103c6d4e2b14e75
Mbarnabas0529 <barnabasmitro@gmail.com>
2022-11-13 10:24:32 +0100

    11.13. hazi 9

---
+++ hazi 9.py
@@ -0,0 +1,9 @@
+t1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2 = ['Január', 'Február', 'Március', 'Április', 'Május', 'Június',  'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December']
+t3 = []
+for i in range(12):
+    t3.append(t2[i])
+    t3.append(t1[i])
+print(t3)
+
+
