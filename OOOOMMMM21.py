67585ed367ab221d959c0f2e5e825689d2012bd4
mitroba <barnabasmitro@gmail.com>
2023-01-11 12:30:29 +0100

    2023.1.11   DIGIT OF LIFE

--- OOOOMMMM21.py
+++ OOOOMMMM21.py
@@ -1,3 +1,4 @@
+#ha nem irok pontot mukodik
 ev = (input("Add meg a születési évedet ÉÉÉÉ.HH.NN-formátumban:"))
 x = 0
 for e in ev:
@@ -14,3 +15,25 @@ else:
             print(ev)
             ev = str(asd)
 print("Digit of Life:", ev)
+
+
+
+#arian feladat:
+def digit(be):
+    x = 0
+    for i in be:
+        if i != ".":
+            i = int(i)
+            x = x + i
+    return str(x)
+a = input("ÉÉÉÉ.HH.NN.: ")
+
+eredmeny = digit(a)
+
+while len(str(eredmeny)) != 1:
+    eredmeny = digit(eredmeny)
+
+print("Keresett szam: ",eredmeny)
+
+print("2. feladat")
+
