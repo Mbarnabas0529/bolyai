d5f7c9440f4e93713ae1e0d67adf605811ef5cbc
mitroba <barnabasmitro@gmail.com>
2023-01-18 12:29:19 +0100

    2023.1.18 fogalmamsincsmianevvvv

--- fogalmamsincsmianev.py
+++ fogalmamsincsmianev.py
@@ -1,25 +1,41 @@
 dictionary = {
               "cat": "cica",
               "dog": "kutya",
-              "horse": "ló"
+              "horse": "ló",
+              "parrot": "papagáj",
+              "bear": "medve",
               }
 words=""
+#dictionary['swan'] = 'hattyú'
 while dictionary != words:
     if words == " ":
         break
+
     words = input("Words: ")
 
     if words in dictionary:
         print(words,"=",dictionary[words])
     else:
         print(words,"=","is not in dictionary")
-print('\n')
-#f2
-dictionary = {
+print(30*"-*")
+dictionary3 = {
               "cat": "cica",
               "dog": "kutya",
               "horse": "ló"
               }
-for key in dictionary.keys():
-    print(key, "->", dictionary[key])
+for i in range(10):
+    new_word = input("Uj szo: ")
+    new_key = input("Uj jelentes: ")
+    dictionary3.update({new_word:new_key})
+print(dictionary3)
+print(30*'-*')
+for english, magyar in dictionary.items():
+    print(english, "->", magyar)
+print(30 * "-*")
+x = dictionary.get(input("Minek a jelentését akarod tudni:"))
+
+print(x)
+
+
+
 
