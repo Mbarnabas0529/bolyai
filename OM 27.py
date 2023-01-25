ea740ffd035316e66fe56bf8935cc1be8bf70916
mitroba <barnabasmitro@gmail.com>
2023-01-25 12:29:25 +0100

    2023.1.25 SZOTARGENYOOOOO

---
+++ OM 27.py
@@ -0,0 +1,11 @@
+for i in range(3):
+    targyak = {
+        input("neved:") : {
+            'magyar': input("Magyar jegyed:"),
+            'matek' : input("Matek jegyed:"),
+            'Tori' :  input("Töri jegyed:"),
+            'Angol' : input("Angol jegyed:"),
+
+                }
+                }
+
