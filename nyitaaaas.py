05104360306464f296a5c0b3710b045929a47e81
mitroba <barnabasmitro@gmail.com>
2023-03-07 09:40:42 +0100

    2023.3.07

--- nyitaaaas.py
+++ nyitaaaas.py
@@ -1,4 +1,14 @@
-with open("enfajlom.txt",'r+',encoding="utf-8") as forras:
-    print(forras.name)
-    print(forras.readline())
-    print(forras.readline())
+orszagok=[]
+with open("eu.csv","r+",encoding="utf-8") as forras:
+    for sor in forras:
+        orszagok_lista=sor.strip().split(",")
+        egy_orszag={"orszag":orszagok_lista[0],"főváros":orszagok_lista[1],"Csatlakozás":orszagok_lista[2]}
+        orszagok.append(egy_orszag)
+print(orszagok)
+for orszag in orszagok:
+    print(orszag['orszag'])
+#for fovaros in orszagok:
+    #print(fovaros['főváros'])
+for csat in orszagok:
+  c =  print(int(csat['Csatlakozás']))
+  print(c)
