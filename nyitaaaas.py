a4cc6d9679b2ff6753febc5091c3961df5910109
mitroba <barnabasmitro@gmail.com>
2023-03-08 12:29:07 +0100

    2023.3.08

--- nyitaaaas.py
+++ nyitaaaas.py
@@ -1,14 +1,16 @@
 orszagok=[]
+csati=[]
+def talalo(_talalok):
+    return _talalok['csatlakozas']
 with open("eu.csv","r+",encoding="utf-8") as forras:
     for sor in forras:
         orszagok_lista=sor.strip().split(",")
-        egy_orszag={"orszag":orszagok_lista[0],"főváros":orszagok_lista[1],"Csatlakozás":orszagok_lista[2]}
+        egy_orszag={"orszag":orszagok_lista[0],"főváros":orszagok_lista[1],"csatlakozas":orszagok_lista[2]}
         orszagok.append(egy_orszag)
 print(orszagok)
 for orszag in orszagok:
     print(orszag['orszag'])
 #for fovaros in orszagok:
     #print(fovaros['főváros'])
-for csat in orszagok:
-  c =  print(int(csat['Csatlakozás']))
-  print(c)
+orszagok.sort(key=talalo)
+print(orszagok)
