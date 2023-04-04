38d5ac8212c8137c66bc2015994aeebacc98e334
mitroba <barnabasmitro@gmail.com>
2023-04-04 09:38:34 +0200

    2023.4.04

---
+++ feladatok.py
@@ -0,0 +1,55 @@
+print("Feladat 1)")
+def ter():
+    a = int(input("a:"))
+    b = int(input("b:"))
+
+    t = a*b
+
+    return t
+
+
+print(f"T:{ter()}")
+print("\n")
+
+print("Feladat 2)")
+def lnko():
+    sz1 = int(input("szám1:"))
+    sz2 = int(input("szám2:"))
+    while sz2:
+        sz1,sz2 = sz2, sz1%sz2
+        return sz1
+print(lnko())
+print("\n")
+
+print("Feladat 3)")
+def oszto():
+    szamlalo = 0
+    f = int(input("szam:"))
+    for i in range()
+print("\n")
+
+print("Feladat 4)")
+def prim():
+    n = int(input("adj meg egy számot:"))
+    if n / n == 1 and n/1 == n:
+        return "prím"
+    else:
+        return "Ez nem prím"
+print(prim())
+print("\n")
+
+print("Feladat 5)")
+print("\n")
+
+print("Feladat 6)")
+print("\n")
+
+print("Feladat"" 7)")
+print("\n")
+
+print("Feladat 8)")
+print("\n")
+
+print("Feladat 9)")
+print("\n")
+
