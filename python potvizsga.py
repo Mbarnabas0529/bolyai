7220c3e8d0f49f9aa4707781c2771030be09e4de
mitroba <barnabasmitro@gmail.com>
2023-04-26 12:23:16 +0200

    2023.4.26

--- python potvizsga.py
+++ python potvizsga.py
@@ -8,21 +8,45 @@ else:
     print(x)
 print('Feladat 2)')
 
-def kodolas():
+def kodolas(_szoveg,_betu):
     muv = ""
-    szoveg = input("Adj meg egy szöveget:")
-    betu = input("Adj meg egy kicserélendő betűt:")
-
     for ltr in szoveg:
         if ltr == betu:
             muv += str(ord(ltr))
         else:
             muv += ltr
-
     return muv
-print(kodolas())
+szoveg = input("Adj meg egy szöveget:")
+betu = input("Adj meg egy kicserélendő betűt:")
+
+print(kodolas(szoveg,betu))
 
 print("feladat 3)")
+autok = []#osszes auto
+egy_auto = []
+ferohely = 0
+_ferohely = 0
+atlag = 0
 with open("autok.csv","r", encoding="utf-8") as f:
-    fejlec = f.readline().strip().split(";")
-    dbszam = len(fejlec)
+    f.readline()
+    for sor in f:
+        egy_auto = sor.strip().split(";")
+        autok.append(egy_auto)
+print(autok)
+print('3B)')
+for egy_auto in autok:
+    if egy_auto[0]=="Munkács" and egy_auto[1]=="Záhony":
+        ferohely+=int(egy_auto[4])
+print(ferohely)
+
+print('3C)')
+for egy_auto in autok:
+    if egy_auto[4]:
+        _ferohely += int(egy_auto[4])
+        atlag = _ferohely/len(autok)
+print(round(atlag,1))
+print('3D')
+with open("budapestrol.dat","w",encoding="utf-8") as b:
+    for egy_auto in autok:
+        if egy_auto[0] == "Budapest":
+            print(";".join(egy_auto),file=b)
